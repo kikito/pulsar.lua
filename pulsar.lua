8a19c1483bf375f9b658f2c94e17cfbe7b914b7c
Enrique García Cota <kikito@gmail.com>
2012-01-23 00:08:11 +0100

    yeeehaaa

--- pulsar.lua
+++ pulsar.lua
@@ -6,6 +6,12 @@
 
 local Path = {}
 
+local function copy(t)
+  local cp = {}
+  for i=1,#t do cp[i] = t[i] end
+  return cp
+end
+
 local pathmt = {
 
   __tostring = function(self)
@@ -20,8 +26,8 @@ local pathmt = {
 
   __eq = function(self, other)
 
-    local myLength, otherLength = #self, #other
-    if myLength ~= otherLength then return false end
+    local myLength = #self
+    if myLength ~= #other then return false end
 
     for i=1, myLength do
       if self[i] ~= other[i] then return false end
@@ -31,8 +37,8 @@ local pathmt = {
   end
 }
 
-function Path:new(...)
-  return setmetatable({...}, pathmt)
+function Path:new(locations)
+  return setmetatable(copy(locations), pathmt)
 end
 
 ----------------------------------------------------------------------------
@@ -55,23 +61,17 @@ end
 local Finder = {}
 local findermt = { __index = Finder }
 
+local function reverse(t)
+  local rev, len = {}, #t
+  for i=1, len do rev[i] = t[len - i - 1] end
+  return rev
+end
+
 local function sortByf(a,b)
   return a.f < b.f
 end
 
-local function sortOpenIfNeeded(self)
-  if not self.openIsSorted then
-    table.sort(self.open, sortByf)
-    self.openIsSorted = true
-  end
-end
-
 local function createNode(self, location, parent, g, h)
-  parent = parent or self.bestNode
-  local parentLocation = parent and parent.location or self.origin
-  g = g or self.cost(parentLocation, location)
-  h = h or self.heuristic(parentLocation, location)
-
   local node = Node:new(location, parent, g, h)
   self.nodes[location] = node
   return node
@@ -82,73 +82,115 @@ local function checkAndSetParam(finder, value, name)
   finder[name] = value
 end
 
-function Finder:findPath()
-  local result = Path:new()
-
-  while self.bestLocation ~= self.destination do
-    self:findNext()
-    table.insert(result, self.bestLocation)
+local function pickNextBestNode(self)
+  if not self.openIsSorted then
+    table.sort(self.open, sortByf)
+    self.openIsSorted = true
   end
 
-  return result
+  self.bestNode = table.remove(self.open, 1)
+  self.openCount = self.openCount - 1
+  return self.bestNode
 end
 
-function Finder:findNext()
-  local neighbors = self.neighbors(self.bestLocation)
-  table.sort(neighbors, self.sort)
-  self.bestLocation = neighbors[1]
+local function getOrCreateNode(self, location)
+  return self.nodes[location] or createNode(self, location, self.bestNode, math.huge, 0)
 end
 
-function Finder:getOrCreateNode(location, parent, g, h)
-  return self.nodes[location] or createNode(self, location, parent, g, h)
+local function openNode(self, node, g)
+  if not node.open then
+    node.open = true
+    self.openCount = self.openCount + 1
+    self.open[self.openCount] = node
+  end
+
+  node.g = g
+  node.h = self.heuristic(node.location, self.destination)
+  node.f = node.g + node.h
+
+  self.openIsSorted = false
+end
+
+local function openNeighbors(self)
+  local bestNode = self.bestNode
+  local bestLocation = bestNode.location
+
+  local neighbors, neighbor, c, g, node
+
+  neighbors = self.neighbors(bestLocation)
+
+  for i=1, #neighbors do
+    neighbor = neighbors[i]
+    c = self.cost(bestLocation, neighbor)
+    g = bestNode.g + c
+    node = getOrCreateNode(self, neighbor)
+
+    if g < node.g then
+      openNode(self, node, g)
+    end
+  end
+end
+
+local function createInitialNode(self)
+  local origin = self.origin
+  local initialNode = createNode(self, origin, nil, 0, self.heuristic(origin, self.destination))
+  initialNode.open = true
+  self.open[1] = initialNode
+  return initialNode
+end
+
+
+function Finder:step()
+  local bestLocation = pickNextBestNode(self).location
+  if bestLocation ~= self.destination then
+    openNeighbors(self)
+  end
+  return bestLocation
+end
+
+function Finder:buildPath()
+  local node = self.bestNode
+  local origin = self.origin
+  local path = { node.location }
+  local count = 1
+  while node.location ~= origin do
+    node = node.parent
+    count = count + 1
+    path[count] = node.location
+  end
+  return Path:new(rev(path))
+end
+
+function Finder:findPath()
+  local destination = self.destination
+  local bestLocation = self.bestNode.location
+
+  while self.openCount > 0 and bestLocation ~= destination do
+    bestLocation = self:step()
+  end
+
+  if bestLocation == destination then
+    return self:buildPath()
+  end
 end
 
 function Finder:new(origin, destination, neighbors, cost, heuristic)
   local finder = {}
-  checkAndSetParam(finder, origin, "origin")
+  checkAndSetParam(finder, origin,      "origin")
   checkAndSetParam(finder, destination, "destination")
-  checkAndSetParam(finder, neighbors, "neighbors")
-  checkAndSetParam(finder, cost, "cost")
-  checkAndSetParam(finder, heuristic, "heuristic")
-
-  finder.bestLocation = origin
-  finder.sort = function(node1, node2) return heuristic(node1, destination) < heuristic(node2, destination) end
+  checkAndSetParam(finder, neighbors,   "neighbors")
+  checkAndSetParam(finder, cost,        "cost")
+  checkAndSetParam(finder, heuristic,   "heuristic")
 
   finder.nodes = {}
   finder.open = {}
+  finder.openCount = 0
 
   setmetatable(finder, findermt)
-  local initialNode = finder:getOrCreateNode(origin, nil, 0, heuristic(origin, destination))
-  finder:openNode(initialNode)
-  finder.bestNode=initialNode
+
+  self.bestNode = createInitialNode(finder)
+
   return finder
-end
-
-
-function Finder:openNode(node)
-  node.open = true
-  table.insert(self.open, node)
-  self.openIsSorted = false
-end
-
-function Finder:closeNode(node)
-  node.open = false
-end
-
-function Finder:pickNextBestNode()
-  sortOpenIfNeeded(self)
-  local node = self.open[1]
-  if node then
-    table.remove(self.open, 1)
-    return node
-  end
-end
-
-function Finder:openNeighbors()
-  local neighbors = self.neighbors(self.bestLocation)
-  for i=1, #neighbors do
-    self:openNode(self:getOrCreateNode(neighbors[i]))
-  end
 end
 
 ----------------------------------------------------------------------------
@@ -158,6 +200,5 @@ local pulsar = {}
 pulsar.Path = Path
 pulsar.Node = Node
 pulsar.Finder = Finder
-
 
 return pulsar
