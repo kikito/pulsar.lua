7ebf609f0235db1e5717d31e04d0ed87b27cfd35
Enrique García Cota <kikito@gmail.com>
2012-02-08 00:13:33 +0100

    moved back everything into grid.lua

---
+++ grid.lua
@@ -0,0 +1,89 @@
+local Grid = {}
+
+local rows, columns = 49,33
+
+local function buildCells()
+  local cells = {}
+  for y=1, columns do
+    cells[y] = {}
+    for x=1, rows do
+      cells[y][x] = {x=x,y=y}
+    end
+  end
+  return cells
+end
+
+function Grid:getCell(x,y)
+  return self.cells[y] and self.cells[y][x]
+end
+
+function Grid:setOrigin(x,y)
+  self.origin = self:getCell(x,y)
+end
+
+function Grid:setDestination(x,y)
+  self.destination = self:getCell(x,y)
+end
+
+function Grid:toggleObstacle(x,y)
+  local cell = self:getCell(x,y)
+  if cell then cell.obstacle = not cell.obstacle end
+end
+
+function Grid:setHighlighted(x,y)
+  self.highlighted = self:getCell(x,y)
+end
+
+function Grid.new()
+  return setmetatable({
+    cells = buildCells(),
+    rows = rows,
+    columns = columns
+  },{
+    __index = Grid,
+    __call = Grid.getCell
+  })
+end
+
+-- screen/grid relationship
+
+Grid.cellWidth = 16
+Grid.cellHeight = 16
+
+local left,top = 8, 66
+
+-- transform a cell's row and column into screen pixel coordinates
+function Grid:grid2screen(x,y)
+  return left + (x-1) * Grid.cellWidth,
+         top  + (y-1) * Grid.cellHeight
+end
+
+-- given a pixel coordinate, return the coordinates of the cell containing it
+function Grid:screen2grid(wx, wy)
+  return math.floor((wx - left) / Grid.cellWidth) + 1,
+         math.floor((wy - top ) / Grid.cellHeight) + 1
+end
+
+-- drawing functions
+
+local function drawCell(self, cell)
+  local x,y = self:grid2screen(cell.x, cell.y)
+  if self.highlighted == cell then
+    love.graphics.setColor(255,255,255,255)
+    love.graphics.setLineWidth(3)
+  else
+    love.graphics.setColor(255, 255, 255, 100)
+    love.graphics.setLineWidth(1)
+  end
+  love.graphics.rectangle('line', x, y, self.cellWidth, self.cellHeight)
+end
+
+function Grid:draw()
+  for x=1, self.rows do
+    for y=1, self.columns do
+      drawCell(self, self:getCell(x,y))
+    end
+  end
+end
+
+return Grid
