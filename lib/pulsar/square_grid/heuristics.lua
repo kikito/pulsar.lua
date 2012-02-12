22b11e33f7801a5e73e3d9ed7abd1eb3f6608dd1
Enrique García Cota <kikito@gmail.com>
2012-02-12 17:33:32 +0100

    added diagonal heuristic

--- lib/pulsar/square_grid/heuristics.lua
+++ lib/pulsar/square_grid/heuristics.lua
@@ -1,9 +1,15 @@
 local heuristics = {}
 
+local max = function(a,b) return a > b and a or b end
 local abs = function(x) return x < 0 and -x or x end
 
 function heuristics.manhattan(cell1, cell2)
   return abs(cell2.x - cell1.x) + abs(cell2.y - cell1.y)
 end
+
+function heuristics.diagonal(cell1, cell2)
+  return max(abs(cell1.x - cell2.x), abs(cell1.y - cell2.y))
+end
+
 
 return heuristics
